d0c486d3745974314b34e01f3b16ec00d4997978
CavalcanteTI <lucasprogramacao.github@gmail.com>
2025-10-22 00:18:01 -0300

    Refactor card data input and output for two cards

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,68 @@
 #include <stdio.h>
 
-
 int main(){
+char Estado;
+char codigodacarta[5];
+char nomedacidade[20];
+int populacao;
+float areakm;
+float pib;
+int numerodepontost;
 
 char Estado2;
 char codigodacarta2[6];
 char nomedacidade2[22];
-int populaçao2;
-float areakm²2;
+int populacao2;
+float areakm2;
 float pib2;
 int numerodepontost2;
 
-printf("====Cadastrando a segunda Carta!====\n");
+
+printf("====Cadastrando a primeira carta!====\n");
+
+printf("Digite uma letra de A - H:\n");
+scanf("%c" , &Estado);
+
+
+printf("Digite o codigo da carta:\n");
+scanf("%s", codigodacarta);
+
+
+printf("Digite o nome da cidade:\n");
+scanf("%s", nomedacidade);
+
+
+printf("Digite o numero da Habitantes da cidade:\n");
+scanf("%d", &populacao);
+
+
+printf("Digite a Área da cidade:\n");
+scanf("%f",&areakm);
+
+
+printf("Digite o Pib:\n");
+scanf("%f", &pib);
+
+
+printf("Digite o numero de pontos Turisticos:\n");
+scanf("%d",&numerodepontost);
+
+printf("===== Dados da Carta 1 =====\n");
+printf("Estado: %c\n" ,Estado);
+printf("Codigo: %s\n", codigodacarta);
+printf("Nome da cidade: %s\n", nomedacidade);
+printf("População: %d\n", populacao);
+printf("Área: %.3f\n", areakm);
+printf("PIB: %.2f bilhoes de reais\n", pib);
+printf("Pontos turisticos: %d", numerodepontost);
+printf("\n");
+
+
+printf("\n====Cadastrando a segunda Carta!====\n");
 
 
 printf("Digite uma letra de A - H:\n");
-scanf("%c" , &Estado2);
+scanf(" %c", &Estado2);
 
 
 printf("Digite o codigo da carta:\n");
@@ -27,11 +74,11 @@ scanf("%s", nomedacidade2);
 
 
 printf("Digite o numero da Habitantes da cidade:\n");
-scanf("%d", &populaçao2);
+scanf("%d", &populacao2);
 
 
 printf("Digite a Área da cidade:\n");
-scanf("%f",&areakm²2);
+scanf("%f",&areakm2);
 
 
 printf("Digite o Pib:\n");
@@ -45,8 +92,8 @@ printf("\n===== Dados da Carta 2 =====\n");
 printf("Estado: %c\n", Estado2);
 printf("Codigo: %s\n", codigodacarta2);
 printf("Nome da cidade: %s\n", nomedacidade2);
-printf("População: %d\n", populaçao2);
-printf("Área: %.3f\n", areakm²2);
+printf("População: %d\n", populacao2);
+printf("Área: %.3f\n", areakm2);
 printf("PIB: %.2f bilhoes de reais\n", pib2);
 printf("Pontos turisticos: %d", numerodepontost2);
 }
