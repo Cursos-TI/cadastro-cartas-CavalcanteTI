a88938281aa3dfbe5939e6f7a4b105e25bd25968
CavalcanteTI <lucasprogramacao.github@gmail.com>
2025-10-23 18:35:43 -0300

    Refactor variable declarations and output formatting

--- CartasSuperTrunfoAventureiro.c
+++ CartasSuperTrunfoAventureiro.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
 int main(){
-char Estado;
-char codigodacarta[5];
-char nomedacidade[20];
-int populacao;
-float areakm;
-float pib;
-int numerodepontost;
-float densidadepopulacional;
+char Estado, codigodacarta[5], nomedacidade[20];
+int populacao, numerodepontost;
+float areakm, densidadepopulacional, pibpercapita, pib;
 
-char Estado2;
-char codigodacarta2[6];
-char nomedacidade2[22];
-int populacao2;
-float areakm2;
-float pib2;
-int numerodepontost2;
-float densidadepopulacional2;
+char Estado2, codigodacarta2[6], nomedacidade2[22];
+int populacao2, numerodepontost2;
+float areakm2, densidadepopulacional2, pibpercapita2, pib2;
 
 
+//==== ADICIONANDO AS INFORMAÇOES PARA A PRIMEIRA CARTA ====//
 
 printf("====Cadastrando a primeira carta!====\n");
 
@@ -32,7 +23,7 @@ scanf("%s", codigodacarta);
 
 
 printf("Digite o nome da cidade:\n");
-scanf("%s", nomedacidade);
+scanf(" %[^\n]", nomedacidade);
 
 
 printf("Digite o numero da Habitantes da cidade:\n");
@@ -50,24 +41,27 @@ scanf("%f", &pib);
 printf("Digite o numero de pontos Turisticos:\n");
 scanf("%d",&numerodepontost);
 
+//==== RESULTADO DAS CARTAS APÓS AS INFORMAÇOES ADICIONADAS ====//
+
 printf("===== Dados da Carta 1 =====\n");
 printf("Estado: %c\n" ,Estado);
 printf("Codigo: %s\n", codigodacarta);
 printf("Nome da cidade: %s\n", nomedacidade);
 printf("População: %d\n", populacao);
-printf("Área: %.3f\n", areakm);
+printf("Área: %.2f km²\n", areakm);
 printf("PIB: %.2f trilhoes de reais\n", pib);
-printf("Pontos turisticos: %d", numerodepontost);
+printf("Numero de Pontos turisticos: %d", numerodepontost);
 printf("\n");
 
+pibpercapita = (float) pib / populacao;
+printf("PIB per Capita: %.2f Reais\n", pibpercapita);
 
 densidadepopulacional = (float) populacao / areakm;
-
-printf("Densidade Populacional de habitantes/km² é : %f\n." , densidadepopulacional);
-
+printf("Densidade Populacional de: %f hab/km²\n", densidadepopulacional);
 
 
 
+//====ADICIONANDO AS INFORMAÇOES INSERIDAS PARA A SEGUNDA CARTA====//
 
 printf("\n====Cadastrando a segunda Carta!====\n");
 
@@ -81,7 +75,7 @@ scanf("%s", codigodacarta2);
 
 
 printf("Digite o nome da cidade:\n");
-scanf("%s", nomedacidade2);
+scanf(" %[^\n]", nomedacidade2);
 
 
 printf("Digite o numero da Habitantes da cidade:\n");
@@ -99,17 +93,19 @@ scanf("%f", &pib2);
 printf("Digite o numero de pontos Turisticos:\n");
 scanf("%d",&numerodepontost2);
 
+//==== RESULTADO DAS CARTAS APÓS AS INFORMAÇOES ADICIONADAS ====//  
+
 printf("\n===== Dados da Carta 2 =====\n");
 printf("Estado: %c\n" , Estado2);
 printf("Codigo: %s\n" , codigodacarta2);
 printf("Nome da cidade: %s\n" , nomedacidade2);
 printf("População: %d\n" , populacao2);
-printf("Área: %.3f\n" , areakm2);
+printf("Área: %.2f km²\n" , areakm2);
 printf("PIB: %.2f bilhoes de reais\n" , pib2);
-printf("Pontos turisticos: %d\n" , numerodepontost2);
+printf("Numero de Pontos turisticos: %d\n" , numerodepontost2);
 
-
+pibpercapita2 = (float) pib2 / populacao2;
 densidadepopulacional2 = (float) populacao2 / areakm2;
-printf("Densidade Populacional de habitantes/km²: %f" , densidadepopulacional2);
+printf("Densidade Populacional de : %.2f hab/km²\n" , densidadepopulacional2);
+printf("PIB per Capita: %.2f Reais\n", pibpercapita2);
 }
-
